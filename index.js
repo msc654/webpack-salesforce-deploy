abec2aa179c67b2c5351295e627615f026bdd74b
John Nelson <john.aaron.nelson@gmail.com>
2016-06-30 19:16:50 -0400

    fixed bugs

--- index.js
+++ index.js
@@ -1,110 +1,117 @@
 var fs = require('fs'),
-  jsforce = require('jsforce'),
-  assetName = '',
-  jsForceConfig;
+    Zip = require('node-zip'),
+    jsforce = require('jsforce'),
+    assetName = '',
+    jsForceConfig;
 
 var WebpackSalesforceDeployPlugin = module.exports = function (options) {
-  //define the incoming options from the webpack.saleforce.deploy.js
-  this.options = options || {};
-  assetName = options.assetName;
-  // get the Org credintials from the jsforce.config.js file.
-  try { jsForceConfig = require(options.jsConfigPath) } catch (e) { }
+    // Define the incoming options from the webpack.saleforce.deploy.js
+    this.options = options || {};
+    assetName = options.assetName;
+    // get the Org credintials from the jsforce.config.js file.
+    try { jsForceConfig = require(options.jsConfigPath); } catch (e) { }
+};
+
+WebpackSalesforceDeployPlugin.prototype.apply = function (compiler) {
+    compiler.plugin('done', this.triggerDeploy.bind(this));
 };
 
 WebpackSalesforceDeployPlugin.prototype.triggerDeploy = function (stats) {
-  var error = '';
-  if (stats.hasErrors()) {
-    error = stats.compilation.errors[0];
-  } else if (this.options.resourceFolderPath !== 'undefined') {
-    var rawDir = this.options.resourcePath;
+    var self = this,
+        error = '',
+        rawDir = this.options.resourcePath;
+
     var dir = rawDir.substring(0, rawDir.indexOf('bundle.js'));
-    fs.readdir(dir, function (err, files) {
-      if (err) throw err;
-      var c = 0;
-      var zipApp = new require('node-zip')();
-      var zipVendor = new require('node-zip')();
-      var zipCommons = new require('node-zip')();
-      // Add files to their respective statuc resources
-      files.forEach(function (file) {
-        // Not too sure why we're itterating a counter
-        c++;
-        var data = fs.readFileSync(dir + file, 'utf8');
-        if (err) throw err;
-        if (file === 'bundle.js') {
-          zipApp.file(file, data);
-        }
-        if (file === 'vendor.js') {
-          zipVendor.file(file, data);
-        }
-        if (file === 'commons.js') {
-          zipCommons.file(file, data);
-        }
-        if (0 === --c) {
-          console.log(file);
-        }
-      });
-      // Default to true
-      if (this.options.copy || this.options.copy === undefined) {
-        var zDataAppBuffer = zipApp.generate({ type: "nodebuffer", compression: "DEFLATE" });
-        var zDataVendorBuffer = zipVendor.generate({ type: "nodebuffer", compression: "DEFLATE" });
-        var zDataCommonsBuffer = zipCommons.generate({ type: "nodebuffer", compression: "DEFLATE" });
+
+    if (stats.hasErrors()) {
+        error = stats.compilation.errors[0];
+        console.log(error);
+    } else if (self.options.resourceFolderPath !== 'undefined') {
+        fs.readdir(dir, function (err, files) {
+            var zipApp = new Zip(),
+                zipVendor = new Zip(),
+                zipCommons = new Zip();
+
+            if (err) throw err;
+            addFilesToStaticResources(zipApp, zipVendor, zipCommons, files);
+            if (self.options.copy || self.options.copy === undefined) {
+                copyStaticResources(zipApp, zipVendor, zipCommons);
+            }
+            if (self.options.deploy || self.options.deploy === undefined) {
+                deployStaticResources(zipApp, zipVendor, zipCommons);
+            }
+        }); // end of directory read
+    } // end of If Statement
+    function addFilesToStaticResources(zipApp, zipVendor, zipCommons, files) {
+        files.forEach(function (file) {
+            var data = fs.readFileSync(dir + file, 'utf8');
+
+            switch (file) {
+                case 'bundle.js':
+                    zipApp.file(file, data);
+                    break;
+                case 'vendor.js':
+                    zipVendor.file(file, data);
+                    break;
+                case 'commons.js':
+                    zipCommons.file(file, data);
+                    break;
+            }
+        });
+    }
+    function deployStaticResources(zipApp, zipVendor, zipCommons) {
+        // Create the connection object
+        var conn = new jsforce.Connection({ loginUrl: jsForceConfig.url || 'https://login.salesforce.com' }),
+            // The base64 data to sent to Salesforce if we are deploying the code
+            zDataApp = zipApp.generate({ base64: true, compression: 'DEFLATE' }),
+            zDataVendor = zipVendor.generate({ base64: true, compression: 'DEFLATE' }),
+            zDataCommons = zipCommons.generate({ base64: true, compression: 'DEFLATE' }),
+            // Create you metadata for the static resource
+            metaDataPayload = [
+                {
+                    fullName: assetName,
+                    content: zDataApp
+                }, {
+                    fullName: 'vendor',
+                    content: zDataVendor
+                }, {
+                    fullName: 'commons',
+                    content: zDataCommons
+                }
+            ].map(function (bundleDefinition) {
+                return {
+                    fullName: bundleDefinition.fullName,
+                    content: bundleDefinition.content,
+                    contentType: 'application/zip',
+                    cacheControl: 'Private'
+                };
+            });
+
+        // Login to the org
+        conn.login(jsForceConfig.username, jsForceConfig.password + jsForceConfig.token, function (err, res) {
+            if (err) throw err;
+            // Actually deploy the Static Resource to Salesforce
+            conn.metadata.upsert('StaticResource', metaDataPayload, function (err, results) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (results[0].success === true) {
+                        console.log('The Static Resouce: ' + assetName + ', vendor and commons was updated!');
+                    } else {
+                        console.log(results);
+                    }
+                }
+            }); // end of upsert
+        }); // end of login
+    }
+    function copyStaticResources(zipApp, zipVendor, zipCommons) {
+        var zDataAppBuffer = zipApp.generate({ type: 'nodebuffer', compression: 'DEFLATE' }),
+            zDataVendorBuffer = zipVendor.generate({ type: 'nodebuffer', compression: 'DEFLATE' }),
+            zDataCommonsBuffer = zipCommons.generate({ type: 'nodebuffer', compression: 'DEFLATE' });
+
         // Copy the zip to the StaticResource folder
         fs.writeFile('./../../src/staticresources/' + assetName + '.resource', zDataAppBuffer, 'binary');
         fs.writeFile('./../../src/staticresources/' + 'vendor' + '.resource', zDataVendorBuffer, 'binary');
         fs.writeFile('./../../src/staticresources/' + 'commons' + '.resource', zDataCommonsBuffer, 'binary');
-      }
-      // Default to true
-      if (this.options.deploy || this.options.deploy === undefined) {
-        var zDataApp = zipApp.generate({ base64: true, compression: 'DEFLATE' });
-        var zDataVendor = zipVendor.generate({ base64: true, compression: 'DEFLATE' });
-        var zDataCommons = zipCommons.generate({ base64: true, compression: 'DEFLATE' });
-        // Create the connection object
-        var conn = new jsforce.Connection({ loginUrl: jsForceConfig.url || 'https://login.salesforce.com' });
-        // Login to the org
-        conn.login(jsForceConfig.username, jsForceConfig.password + jsForceConfig.token, function (err, res) {
-          // Create you metadata for the static resource
-          var metaDataPayload = getPayload(
-            [
-              {
-                fullName: assetName,
-                content: zDataApp
-              }, {
-                fullName: 'vendor',
-                content: zDataVendor
-              }, {
-                fullName: 'commons',
-                content: zDataCommons
-              }
-            ]
-          );
-          // Actually deploy the Static Resource to Salesforce
-          conn.metadata.upsert('StaticResource', metaDataPayload, function (err, results) {
-            if (err) {
-              console.log(err);
-            } else {
-              if (results[0].success === true) {
-                console.log('The Static Resouce: ' + assetName + ', vendor and commons was updated!');
-              } else {
-                console.log(results);
-              }
-            }
-          }); // end of upsert
-        }); // end of login
-      }
-    }); // end of directory read
-  } // end of If Statement
-  function getPayload(bundleDefinitions) {
-    return bundleDefinitions.map(function (bundleDefinition) {
-      return {
-        fullName: bundleDefinition.fullName,
-        content: bundleDefinition.content,
-        contentType: 'application/zip',
-        cacheControl: 'Private'
-      }
-    })
-  }
-};
-
-WebpackSalesforceDeployPlugin.prototype.apply = function (compiler) {
-  compiler.plugin('done', this.triggerDeploy.bind(this));
+    }
 };
